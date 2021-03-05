9378a848babb34780e3684a63c7a14e68bf2ad9f
unknown <oscarpadilla1907@gmail.com>
2021-03-05 21:05:20 +0100

    Login

---
+++ firebaselogin-react/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Te has logeado</h1>
+            <h2>Gabri apruebame</h2>
+        </div>
+    )
+}
+
+
+
+export default Home
